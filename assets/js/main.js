c7db2ae5fbba6008b7353de41a978303a950434b
TestNguyen <91844851+TestNguyen@users.noreply.github.com>
2023-03-22 20:55:43 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+// let images = [
+//   "./assets/img/middle-banner1.png",
+//   "./assets/img/middle-banner2.png",
+//   "./assets/img/middle-banner3.png",
+// ];
+
+// let index = 0;
+// const imgElement = document.querySelector("#mainPhoto");
+
+// function change() {
+//   imgElement.src = images[index];
+//   index > 1 ? (index = 0) : index++;
+// }
+
+// window.onload = function () {
+//   setInterval(change, 1000);
+// };
+
+
+// --------------sticky navbar------------
+
+
+// window.onscroll = function () {
+//   myFunction();
+// };
+
+// var navbar = document.querySelector("main-menu");
+// var sticky = navbar.offsetTop;
+
+// function myFunction() {
+//   if (window.pageYOffset >= sticky) {
+//     navbar.classList.add("sticky");
+//   } else {
+//     navbar.classList.remove("sticky");
+//   }
+// }
